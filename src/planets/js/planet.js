85eb90822d32df056e8d4648709537627fcf492f
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-05-23 05:41:23 -0700

    Added planetary bodies

--- src/planets/js/planet.js
+++ src/planets/js/planet.js
@@ -6,6 +6,41 @@ var controller = {
     "cube"      : null,
 };
 
+// Sun radius in km, 1 R
+var sun = {
+    "radius" : 695500
+}
+
+// Planet radii, distances, masses in km
+var bodies_data = {
+    "mercury"   : {
+        "radius" : 2439.7                
+    },
+    "venus"     : {
+        "radius" : 6051.8
+    },
+    "earth"     : {
+        "radius" : 6371
+    },
+    "mars"      : {
+        "radius" : 3389.5
+    },
+    "jupiter"   : {
+        "radius" : 69911
+    },
+    "saturn"    : {
+        "radius" : 58232
+    },
+    "uranus"    : {
+        "radius" : 25362
+    },
+    "neptune"   : {
+        "radius" : 24622
+    }
+};
+
+var bodies = {};
+
 function init() {
     // Load scene, camera, renderer
     createScene();
@@ -20,9 +55,11 @@ function render() {
     window.requestAnimationFrame(render);
 
     // Animation Starts
-    
-    controller["sphere"].rotation.x += 0.1; 
-    controller["sphere"].rotation.y += 0.1;
+   
+    sun["display"].rotation.x += 10;
+    sun["display"].rotation.y += 10;
+    //controller["sphere"].rotation.x += 0.1; 
+    //controller["sphere"].rotation.y += 0.1;
 
     // Animation Ends
 
@@ -34,20 +71,67 @@ function render() {
 render();
 
 
+// createPlanets
+// Initializes planets of relatively correct size
+function createBodies() {
+    // Compute total solar system size [NAIVE]
+    var naiveSolarSystemWidth = sun["radius"]*2 + 200;
+    for (planet in bodies_data) {
+        naiveSolarSystemWidth += 2*bodies_data[planet]["radius"] + 100;
+    }
+
+    // Create sun
+    sun["display"] = new THREE.Mesh(
+        new THREE.SphereGeometry(
+            (sun["radius"]/5)*(window.innerWidth/naiveSolarSystemWidth),
+            40,
+            40
+        ),
+        new THREE.MeshLambertMaterial( { color : 0xFFB00F } )
+    );
+    sun["display"].position.x = -window.innerWidth/2 ;
+    sun["display"].position.y = 0;
+    controller["scene"].add(sun["display"]);
+    var sunPL = new THREE.PointLight(0xFFFFFF);
+    sunPL.position.x = -window.innerWidth/2;
+    sunPL.position.y = 10;
+    sunPL.position.z = 130;
+    controller["scene"].add( sunPL );
+
+    // Create planets
+    var counter = 1;
+    for (planet in bodies_data) {
+        var displayRadius = bodies_data[planet]["radius"]*(window.innerWidth/naiveSolarSystemWidth)
+        bodies[planet] = new THREE.Mesh(
+            new THREE.SphereGeometry(
+                displayRadius/5,
+                16,
+                16
+            ),
+            new THREE.MeshLambertMaterial( { color : 0x00FF00 } )
+        );
+        bodies[planet].position.x = 0;// -window.innerWidth/2 + sun["radius"]*2 + 600;
+        controller["scene"].add( bodies[planet] );
+        counter += 1;
+    }
+}
+
+
 // createScene
 // Loads a THREE.js scene, perspective camera, and renderer into canvas.
 // Currently adds a sphere
 function createScene () {
     controller["scene"] = new THREE.Scene(); 
     controller["camera"] = new THREE.PerspectiveCamera( 
-        45, window.innerWidth / window.innerHeight, 0.1, 10000
+        45, window.innerWidth / window.innerHeight, 1, 10000
     );  
     controller["renderer"] = new THREE.WebGLRenderer(); 
     controller["renderer"].setSize( window.innerWidth, window.innerHeight );  
-    controller["camera"].position.z = 300;
+    controller["camera"].position.z = 1000;
     document.body.appendChild( controller["renderer"].domElement );
    
-    var geo = new THREE.SphereGeometry(
+    createBodies();
+    /*var geo = new THREE.SphereGeometry(
         50, 
         16, 
         16  
@@ -59,7 +143,10 @@ function createScene () {
     controller["sphere"].geometry.dynamic = true;
     controller["sphere"].geometry.verticesNeedUpdate = true;
     controller["sphere"].geometry.normalsNeedUpdate = true;
-    controller["scene"].add( controller["sphere"] );
+    controller["scene"].add( controller["sphere"] );*/
+
+    var ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    //controller["scene"].add(ambientLight);
 
     var pointLight = new THREE.PointLight(0xFFFFFF);
     pointLight.position.x = 10;
